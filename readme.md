e291b0903785a6949f02cc72987ebe7eeafebceb
Francisco Javier Barba <barbatrejofco@gmail.com>
2023-12-28 19:47:06 +0100

    fix: Update readme.md with project instructions and change the controller tests

--- readme.md
+++ readme.md
@@ -2,6 +2,50 @@
 
 Este proyecto implementa un servicio en SpringBoot que ofrece un endpoint REST para consultar precios en la base de datos de comercio electrónico de la compañía. La información se extrae de la tabla `PRICES`, que refleja el precio final (PVP) y la tarifa que se aplica a un producto de una cadena en un rango de fechas determinado.
 
+## Cómo Ejecutar el Proyecto
+
+1. Clona este repositorio: `git clone https://github.com/barbaat/bcnc-inditex.git`.
+2. Abre el proyecto en tu IDE favorito.
+3. Ejecuta `mvn clean install` para isntalar todas las despendencias y compilar la aplicación.
+3. Ejecuta la aplicación SpringBoot con Maven: `mvn spring-boot:run`.
+4. Accede al endpoint REST para realizar consultas de precios.
+
+## Ejemplos de Test
+
+Se han desarrollado tests para el endpoint REST que validan las siguientes peticiones al servicio con los datos del ejemplo:
+
+- **Test 1:** Petición a las 10:00 del día 14 para el producto 35455 y la cadena 1 (ZARA).
+- **Test 2:** Petición a las 16:00 del día 14 para el producto 35455 y la cadena 1 (ZARA).
+- **Test 3:** Petición a las 21:00 del día 14 para el producto 35455 y la cadena 1 (ZARA).
+- **Test 4:** Petición a las 10:00 del día 15 para el producto 35455 y la cadena 1 (ZARA).
+- **Test 5:** Petición a las 21:00 del día 16 para el producto 35455 y la cadena 1 (ZARA).
+
+## PriceControllerTests
+
+Esta clase contiene pruebas unitarias para la clase PriceController. Incluye una prueba para el método `getPriceValue`, que prueba la solicitud GET al endpoint `/price/get-price`.
+
+### Configuración
+
+- Declaraciones de Autowired y MockBean para PriceController, PriceService, ProductService y BrandService.
+- Objetos Price y un método de configuración para datos de prueba.
+- Método de utilidad para crear objetos Price.
+
+### Cómo Ejecutar
+
+Ejecute las pruebas utilizando su marco de pruebas preferido.
+
+## PricesServicesTests
+
+Esta clase contiene pruebas unitarias para la clase PriceService. Incluye una prueba para el método `getPriceByDatesAndProductAndBrand`.
+
+### Configuración
+
+- Declaraciones de Mock e InjectMocks para PriceRepository y PriceService.
+
+### Cómo Ejecutar
+
+Ejecute las pruebas utilizando su marco de pruebas preferido.
+
 ## Tabla PRICES
 
 La tabla PRICES contiene los siguientes campos relevantes:
@@ -34,21 +78,5 @@ Los datos de salida incluyen:
 
 Se utiliza una base de datos en memoria de tipo H2, inicializada con los datos del ejemplo. Se ha elegido el tipo de dato que se considera adecuado para cada campo.
 
-## Ejemplos de Test
 
-Se han desarrollado tests para el endpoint REST que validan las siguientes peticiones al servicio con los datos del ejemplo:
-
-- **Test 1:** Petición a las 10:00 del día 14 para el producto 35455 y la cadena 1 (ZARA).
-- **Test 2:** Petición a las 16:00 del día 14 para el producto 35455 y la cadena 1 (ZARA).
-- **Test 3:** Petición a las 21:00 del día 14 para el producto 35455 y la cadena 1 (ZARA).
-- **Test 4:** Petición a las 10:00 del día 15 para el producto 35455 y la cadena 1 (ZARA).
-- **Test 5:** Petición a las 21:00 del día 16 para el producto 35455 y la cadena 1 (ZARA).
-
-## Cómo Ejecutar el Proyecto
-
-1. Clona este repositorio: `git clone https://github.com/barbaat/bcnc-inditex.git`.
-2. Abre el proyecto en tu IDE favorito.
-3. Ejecuta `mvn clean install` para isntalar todas las despendencias y compilar la aplicación.
-3. Ejecuta la aplicación SpringBoot con Maven: `mvn spring-boot:run`.
-4. Accede al endpoint REST para realizar consultas de precios.
 
--- src/test/java/org/springframework/samples/inditex/controllers/PricesControllerTests.java
+++ src/test/java/org/springframework/samples/inditex/controllers/PricesControllerTests.java
@@ -2,6 +2,7 @@ package org.springframework.samples.inditex.controllers;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Collections;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,82 +25,153 @@ import static org.mockito.BDDMockito.given;
 @WebMvcTest(PriceController.class)
 class PricesControllerTests {
 
-    @Autowired
-    private MockMvc mockMvc;
+        @Autowired
+        private MockMvc mockMvc;
 
-    @MockBean
-    private PriceService priceService;
+        @MockBean
+        private PriceService priceService;
 
-    @MockBean
-    private ProductService productService;
+        @MockBean
+        private ProductService productService;
 
-    @MockBean
-    private BrandService brandService;
+        @MockBean
+        private BrandService brandService;
 
-    private Price price1;
-    private Price price2;
-    private Price price3;
-    private Price price4;
+        private Price price1;
+        private Price price2;
+        private Price price3;
+        private Price price4;
 
-    @BeforeEach
-    void setup() {
-        // Creating a Brand object and a Product object
-        Brand brand = new Brand();
-        brand.setId(1);
-        brand.setName("Zara");
-        brand = brandService.createBrand(brand);
+        @BeforeEach
+        void setup() {
+                // Creating a Brand object and a Product object
+                Brand brand = new Brand();
+                brand.setId(1);
+                brand.setName("Zara");
+                brand = brandService.createBrand(brand);
 
-        Product product = new Product();
-        product.setId(35455);
-        product.setName("Shirt");
-        product = productService.createProduct(product);
+                Product product = new Product();
+                product.setId(35455);
+                product.setName("Shirt");
+                product = productService.createProduct(product);
 
-        // Creating Price objects using the createPrice method
-        price1 = createPrice(1, 35.50, LocalDateTime.of(2020, 04, 14, 0, 0),
-                LocalDateTime.of(2020, 12, 31, 23, 59), brand, product);
+                // Creating Price objects using the createPrice method
+                price1 = createPrice(1, 35.50, LocalDateTime.of(2020, 04, 14, 0, 0),
+                                LocalDateTime.of(2020, 12, 31, 23, 59), brand, product);
 
-        price2 = createPrice(2, 25.45, LocalDateTime.of(2020, 6, 14, 15, 0),
-                LocalDateTime.of(2020, 6, 14, 18, 30), brand, product);
+                price2 = createPrice(2, 25.45, LocalDateTime.of(2020, 6, 14, 15, 0),
+                                LocalDateTime.of(2020, 6, 14, 18, 30), brand, product);
 
-        price3 = createPrice(3, 30.50, LocalDateTime.of(2020, 6, 15, 0, 0),
-                LocalDateTime.of(2020, 6, 15, 11, 0), brand, product);
+                price3 = createPrice(3, 30.50, LocalDateTime.of(2020, 6, 15, 0, 0),
+                                LocalDateTime.of(2020, 6, 15, 11, 0), brand, product);
 
-        price4 = createPrice(4, 38.95, LocalDateTime.of(2020, 6, 15, 16, 0),
-                LocalDateTime.of(2020, 6, 15, 23, 59), brand, product);
+                price4 = createPrice(4, 38.95, LocalDateTime.of(2020, 6, 15, 16, 0),
+                                LocalDateTime.of(2020, 6, 15, 23, 59), brand, product);
 
-        // Configuring mock behavior for the PriceService
-        given(priceService.getAll()).willReturn(Arrays.asList(price1, price2, price3, price4));
-        given(priceService.getPriceByDateAndProductAndBrand(LocalDateTime.of(2020, 8, 1, 12, 0), 35455, 1))
-                .willReturn(Arrays.asList(price1));
-    }
+                // Configuring mock behavior for the PriceService
+                given(priceService.getAll()).willReturn(Arrays.asList(price1, price2, price3, price4));
 
-    private Price createPrice(int id, double price, LocalDateTime startDateTime, LocalDateTime endDateTime,
-            Brand brand, Product product) {
-        // Utility method for creating Price objects
-        Price priceObj = new Price();
-        priceObj.setId(id);
-        priceObj.setBrand(brand);
-        priceObj.setProduct(product);
-        priceObj.setStart_dat(startDateTime);
-        priceObj.setEnd_dat(endDateTime);
-        priceObj.setPrice_list(id);
-        priceObj.setPriority(1);
-        priceObj.setPrice(price);
-        priceObj.setCurrency("EUR");
-        return priceObj;
-    }
+                // Test 1: request at 10:00 on the 14th for product 35455 for brand 1 (ZARA)
+                given(priceService.getPriceByDateAndProductAndBrand(LocalDateTime.of(2020, 4, 14, 10, 0), 35455, 1))
+                                .willReturn(Arrays.asList(price1));
 
-    @Test
-    void getPriceValue() throws Exception {
-        // Testing GET request to the /price/get-price endpoint
-        mockMvc.perform(MockMvcRequestBuilders.get("/price/get-price")
-                .param("date", "2020-08-01T12:00:00")
-                .param("productId", "35455")
-                .param("brandId", "1")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.jsonPath("$").exists())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.price").value(35.50))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.currency").value("EUR"));
-    }
+                // Test 2: request at 16:00 on the 14th for product 35455 for brand 1 (ZARA)
+                given(priceService.getPriceByDateAndProductAndBrand(LocalDateTime.of(2020, 6, 14, 16, 0), 35455, 1))
+                                .willReturn(Arrays.asList(price2));
+
+                // Test 3: request at 21:00 on the 14th for product 35455 for brand 1 (ZARA)
+                given(priceService.getPriceByDateAndProductAndBrand(LocalDateTime.of(2020, 6, 14, 21, 0), 35455, 1))
+                                .willReturn(Arrays.asList(price3));
+
+                // Test 4: request at 10:00 on the 15th for product 35455 for brand 1 (ZARA)
+                given(priceService.getPriceByDateAndProductAndBrand(LocalDateTime.of(2020, 6, 15, 10, 0), 35455, 1))
+                                .willReturn(Arrays.asList(price4));
+
+                // Test 5: request at 21:00 on the 16th for product 35455 for brand 1 (ZARA)
+                given(priceService.getPriceByDateAndProductAndBrand(LocalDateTime.of(2020, 6, 16, 21, 0), 35455, 1))
+                                .willReturn(Collections.emptyList());
+        }
+
+        private Price createPrice(int id, double price, LocalDateTime startDateTime, LocalDateTime endDateTime,
+                        Brand brand, Product product) {
+                // Utility method for creating Price objects
+                Price priceObj = new Price();
+                priceObj.setId(id);
+                priceObj.setBrand(brand);
+                priceObj.setProduct(product);
+                priceObj.setStart_dat(startDateTime);
+                priceObj.setEnd_dat(endDateTime);
+                priceObj.setPrice_list(id);
+                priceObj.setPriority(1);
+                priceObj.setPrice(price);
+                priceObj.setCurrency("EUR");
+                return priceObj;
+        }
+
+        @Test
+        void getPriceValueAt10AMOn14th() throws Exception {
+                // Test 1: request at 10:00 on the 14th for product 35455 for brand 1 (ZARA)
+                mockMvc.perform(MockMvcRequestBuilders.get("/price/get-price")
+                                .param("date", "2020-04-14T10:00:00")
+                                .param("productId", "35455")
+                                .param("brandId", "1")
+                                .accept(MediaType.APPLICATION_JSON))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$").exists())
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.price").value(35.50))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.currency").value("EUR"));
+        }
+
+        @Test
+        void getPriceValueAt4PMOn14th() throws Exception {
+                // Test 2: request at 16:00 on the 14th for product 35455 for brand 1 (ZARA)
+                mockMvc.perform(MockMvcRequestBuilders.get("/price/get-price")
+                                .param("date", "2020-06-14T16:00:00")
+                                .param("productId", "35455")
+                                .param("brandId", "1")
+                                .accept(MediaType.APPLICATION_JSON))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$").exists())
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(2))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.price").value(25.45))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.currency").value("EUR"));
+        }
+
+        @Test
+        void getPriceValueAt9PMOn14th() throws Exception {
+                // Test 3: request at 21:00 on the 14th for product 35455 for brand 1 (ZARA)
+                mockMvc.perform(MockMvcRequestBuilders.get("/price/get-price")
+                                .param("date", "2020-06-14T21:00:00")
+                                .param("productId", "35455")
+                                .param("brandId", "1")
+                                .accept(MediaType.APPLICATION_JSON))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$").exists())
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(3))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.price").value(30.50))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.currency").value("EUR"));
+        }
+
+        @Test
+        void getPriceValueAt10AMOn15th() throws Exception {
+                // Test 4: request at 10:00 on the 15th for product 35455 for brand 1 (ZARA)
+                mockMvc.perform(MockMvcRequestBuilders.get("/price/get-price")
+                                .param("date", "2020-06-15T10:00:00")
+                                .param("productId", "35455")
+                                .param("brandId", "1")
+                                .accept(MediaType.APPLICATION_JSON))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$").exists())
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(4))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.price").value(38.95))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.currency").value("EUR"));
+        }
+
+        @Test
+        void getPriceNotFoundAt9PMOn16th() throws Exception {
+                // Test 5: request at 21:00 on the 16th for product 35455 for brand 1 (ZARA)
+                mockMvc.perform(MockMvcRequestBuilders.get("/price/get-price")
+                                .param("date", "2020-06-16T21:00:00")
+                                .param("productId", "35455")
+                                .param("brandId", "1")
+                                .accept(MediaType.APPLICATION_JSON))
+                                .andExpect(MockMvcResultMatchers.status().isNotFound());
+        }
 }
